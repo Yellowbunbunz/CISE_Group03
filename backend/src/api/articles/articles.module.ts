9b0425144950439710d618cfcd2c7fb97f32dd88
MaxwellHinton <qwv9850@autuni.ac.nz>
2024-09-26 13:12:30 +1200

    added articles.modules.ts. HTTP methods work.

---
+++ backend/src/api/articles/articles.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ArticlesService } from './articles.service';
+import { ArticlesController } from './articles.controllers';
+import { Article, ArticleSchema } from './articles.schema';
+
+@Module({
+    imports:[
+        MongooseModule.forFeature([{name: Article.name, schema: ArticleSchema}]),
+    ],
+    providers: [ArticlesService],
+    controllers: [ArticlesController],
+})
+
+export class ArticlesModule {}
